ec3e60d8f4d0d0f3f1a44b36213252639718ccb3
Prasanth1404 <sgprasanth2523@gmail.com>
2025-08-01 20:19:34 +0000

    song upload

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+### Must install ###
+#python3 -m venv venv
+#source venv/bin/activate
+#pip install flask
+#python3 app.py
+
+
 from flask import Flask, render_template, request, send_from_directory
 import os
 from werkzeug.utils import secure_filename
@@ -30,4 +37,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True):
+    app.run(debug=True)
